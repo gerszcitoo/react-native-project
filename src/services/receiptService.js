2e444129c584a7c2894d78f154a486619cef8bd7
gerszcitoo <germands2013@gmail.com>
2024-11-23 21:39:10 -0300

    moved keys to .env

--- src/services/receiptService.js
+++ src/services/receiptService.js
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { base_url } from "../firebase/database";
 
 export const receiptApi = createApi({
   reducerPath: "receiptsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: base_url }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.EXPO_PUBLIC_BASE_URL }),
   endpoints: (builder) => ({
     postReceipt: builder.mutation({
       query: ({ ...receipt }) => ({
