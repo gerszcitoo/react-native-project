ac923aed3acf2f66e1a27d1777fb40a6f25c6823
gerszcitoo <germands2013@gmail.com>
2024-11-24 12:06:27 -0300

    started to work sqlite and modified login/register

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-//clase 04/11 00.00.00
+//clase 06/11 00.40.47
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import MainNavigator from "./src/navigation/MainNavigator";
 import { store } from "./src/app/store";
 import { Provider } from "react-redux";
+import { createSessionsTable } from "./src/db";
+
+createSessionsTable()
+  .then((result) => console.log("tabla creada con exito", result))
+  .catch((error) => console.log("error al crear la tabla", error));
 
 SplashScreen.preventAutoHideAsync();
 
